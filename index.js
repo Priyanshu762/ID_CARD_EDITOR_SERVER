3ee052edb41b748415ad5b160ee5a03f05f02507
Priyanshu Kumar <kumarpriyanshu762@gmail.com>
2025-11-08 12:11:01 +0530

    whitelist

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  { origin: '*' }
+));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
